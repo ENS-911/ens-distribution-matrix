1f99420fe24dc783d4805d5e0b617f115bf2bd71
Kenneth Brook <kenneth.brook@hotmail.com>
2023-12-06 11:22:35 -0500

    initial

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+// Load environment variables
+const {
+  DB_USER,
+  DB_HOST,
+  DB_DATABASE,
+  DB_PASSWORD,
+  DB_PORT,
+} = process.env;
+
+// PostgreSQL connection pool
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
+
+app.get('/client/:clientKey', async (req, res) => {
+  const clientKey = req.params.clientKey;
+
+  try {
+    const client = await pool.query('SELECT * FROM clients WHERE key = $1', [clientKey]);
+
+    if (client.rows.length === 0) {
+      res.status(404).json({ error: 'Client not found' });
+    } else {
+      res.json(client.rows[0]);
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
