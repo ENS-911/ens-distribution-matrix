4cba72e39fcbe8270836dd0a699f215b4ee204bc
Kenneth Brook <kenneth.brook@hotmail.com>
2024-10-24 10:32:16 -0400

    pool2 endpoint corection

--- index.js
+++ index.js
@@ -21,20 +21,6 @@ const pool = new Pool({
   },
 });
 
-let clientKey;
-
-const pool2 = new Pool({
-  user: 'ensahost_client',
-  host: `client-${clientKey}.cfzb4vlbttqg.us-east-2.rds.amazonaws.com`,
-  database: 'postgres',
-  password: 'ZCK,tCI8lv4o',
-  port: 5432,
-  max: 20,
-  ssl: {
-    rejectUnauthorized: false, // Ignore unauthorized SSL errors (not recommended for production)
-  },
-});
-
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -46,7 +32,7 @@ app.get('/', (req, res) => {
   });
 
 app.get('/client/:clientKey', async (req, res) => {
-  clientKey = req.params.clientKey;
+  const clientKey = req.params.clientKey;
 
   try {
     const client = await pool.query('SELECT * FROM clients WHERE key = $1', [clientKey]);
@@ -63,7 +49,19 @@ app.get('/client/:clientKey', async (req, res) => {
 });
 
 app.get('/data/:clientKey', async (req, res) => {
-    clientKey = req.params.clientKey;
+    const clientKey = req.params.clientKey;
+
+    const pool2 = new Pool({
+      user: 'ensahost_client',
+      host: `client-${clientKey}.cfzb4vlbttqg.us-east-2.rds.amazonaws.com`,
+      database: 'postgres',
+      password: 'ZCK,tCI8lv4o',
+      port: 5432,
+      max: 20,
+      ssl: {
+        rejectUnauthorized: false, // Ignore unauthorized SSL errors (not recommended for production)
+      },
+    });
   
     try {
       const client = await pool2.query(`SELECT * FROM client_data_${year} WHERE active = 'yes'`);
@@ -80,7 +78,19 @@ app.get('/data/:clientKey', async (req, res) => {
 });
 
 app.get('/count/:clientKey', async (req, res) => {
-    clientKey = req.params.clientKey;
+    const clientKey = req.params.clientKey;
+
+    const pool2 = new Pool({
+      user: 'ensahost_client',
+      host: `client-${clientKey}.cfzb4vlbttqg.us-east-2.rds.amazonaws.com`,
+      database: 'postgres',
+      password: 'ZCK,tCI8lv4o',
+      port: 5432,
+      max: 20,
+      ssl: {
+        rejectUnauthorized: false, // Ignore unauthorized SSL errors (not recommended for production)
+      },
+    });
   
     try {
         // Get count for the current date
@@ -107,7 +117,19 @@ app.get('/count/:clientKey', async (req, res) => {
     });
 
 app.get('/today/:clientKey', async (req, res) => {
-    clientKey = req.params.clientKey;
+    const clientKey = req.params.clientKey;
+
+    const pool2 = new Pool({
+      user: 'ensahost_client',
+      host: `client-${clientKey}.cfzb4vlbttqg.us-east-2.rds.amazonaws.com`,
+      database: 'postgres',
+      password: 'ZCK,tCI8lv4o',
+      port: 5432,
+      max: 20,
+      ssl: {
+        rejectUnauthorized: false, // Ignore unauthorized SSL errors (not recommended for production)
+      },
+    });
     
       try {
           // Fetch all incidents created today
